eec50419a0caf45c11e49f9479f2935ddd8d1fe9
Marco Mapelli <marco.mapelli@tasgroup.eu>
2025-09-24 12:53:56 +0200

    Update AlixApplication.java

--- AlixApplication.java
+++ AlixApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class QuartzApplication {
+public class AlixApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(QuartzApplication.class, args);
+		SpringApplication.run(AlixApplication.class, args);
 	}
 
 }
